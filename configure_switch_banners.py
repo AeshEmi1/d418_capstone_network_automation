f8080e48b64b7c75ed7e29ab2bd7ead52734ae73
Emiliano <121838259+AeshEmi1@users.noreply.github.com>
2024-06-17 03:45:54 +0000

    Update configure_switch_banners.py

--- configure_switch_banners.py
+++ configure_switch_banners.py
@@ -25,8 +25,12 @@ class SwitchConfiguration:
         """Method for part C1. Returns VLANs as an array."""
         if self.switch_connection:
             # Run command to show vlans
-            banner = "WARNING: This system is monitored\. Unauthorized acceess to this system is forbidden and will be prosecuted by law\."
-            show_vlan = self.switch_connection.send_config_set(f"configure banner \n{banner}\n")
+            banner = [
+                "configure banner",
+                "WARNING: This system is monitored. Unauthorized acceess to this system is forbidden and will be prosecuted by law.",
+                "\n"                
+            ]
+            show_vlan = self.switch_connection.send_config_set(banner)
             self.switch_connection.save_config()
 def main():
     try:
