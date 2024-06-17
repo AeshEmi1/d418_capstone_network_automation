1094ce94c99e170be1855aaadd80090d871a9699
Emiliano <121838259+AeshEmi1@users.noreply.github.com>
2024-06-17 06:31:15 +0000

    Update configure_switch_banners.py

--- configure_switch_banners.py
+++ configure_switch_banners.py
@@ -29,8 +29,8 @@ class SwitchConfiguration:
                 self.switch_connection.send_command("enable cli prompting")
                 show_vlan = self.switch_connection.send_config_set(f"conf banner bef\n{banner}\n\n\n")
                 self.switch_connection.save_config()
-                print(f"Banner configured on {self.switch_ip}")
         except:
+            print(f"Banner configured on {self.switch_ip}")
             pass
 def main():
     try:
