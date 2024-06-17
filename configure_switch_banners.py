8fda823b7198dfd9f72b279ae7cdbb1c50364159
Emiliano <121838259+AeshEmi1@users.noreply.github.com>
2024-06-17 03:55:45 +0000

    Update configure_switch_banners.py

--- configure_switch_banners.py
+++ configure_switch_banners.py
@@ -26,11 +26,10 @@ class SwitchConfiguration:
         if self.switch_connection:
             # Run command to show vlans
             banner = [
+                "enable cli prompting",
                 "configure banner before-login",
-                "\n",
                 "WARNING: This system is monitored. Unauthorized acceess to this system is forbidden and will be prosecuted by law.",
-                "\n",
-                "\n" 
+                "\n"
             ]
             show_vlan = self.switch_connection.send_config_set(banner)
             self.switch_connection.save_config()
